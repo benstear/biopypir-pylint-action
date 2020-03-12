f56e891a27c1a58e11a09113375362ed375a1143
ben stear <bjs385@drexel.edu>
2020-03-12 10:54:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
+echo $0
+echo $1
+
 sh -c "$*"
 
 echo "#################################################"
