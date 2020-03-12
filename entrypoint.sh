ae1355768fce2040e992c132ea6ed636c4d31b73
ben stear <bjs385@drexel.edu>
2020-03-12 18:42:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
-chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
+if [ $0 == "test" ] ; then  chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
+#chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
