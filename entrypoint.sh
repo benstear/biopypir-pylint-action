22b13b48cd4c1e2ae38b8254db49f07993ab7b9a
ben stear <bjs385@drexel.edu>
2020-03-12 15:54:45 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,14 @@ set -e
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-ls -a
-pylint scedar  --exit-zero --reports=y | \
-awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-ls
+#ls -a
+#pylint scedar  --exit-zero --reports=y | \
+#awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
+#echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+#ls
 
+chmod +x /docs/biopypir_docs/biopypir_lint.sh
+/docs/biopypir_docs/biopypir_lint.sh
+pwd
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
