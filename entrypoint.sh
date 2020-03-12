c59b68a7e131b198c55079ad3f3fed73a230400c
ben stear <bjs385@drexel.edu>
2020-03-12 13:30:52 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,14 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 #echo Variable one: $1
 #echo "$*"
 #sh -c "$*"
-PWD
+#PWD
+#ls -a
+#chmod +x ./biopypir_linting.sh
+#./biopypir_linting.sh
+
 ls -a
-chmod +x ./biopypir_linting.sh
-./biopypir_linting.sh
+pylint scedar  --exit-zero --reports=y | \
+awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
