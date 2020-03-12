19f027589b6dcb3f1d9455728385d1991cacbc3f
ben stear <bjs385@drexel.edu>
2020-03-12 13:41:59 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 ls -a
 pylint scedar  --exit-zero --reports=y | \
 awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ls
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
