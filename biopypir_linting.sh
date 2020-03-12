b036fb77e99a4231716d1c6706393e805a117ee4
ben stear <bjs385@drexel.edu>
2020-03-12 13:30:15 -0400

    Update biopypir_linting.sh

--- biopypir_linting.sh
+++ biopypir_linting.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 
-#!/bin/sh
-pwd
 ls -a
 pylint scedar  --exit-zero --reports=y | \
 awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
