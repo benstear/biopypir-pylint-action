1cbc73e96ff25e528a72755af8da039fe2919935
ben stear <bjs385@drexel.edu>
2020-03-13 10:13:21 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 #if [ "$1" == "test" ] 
 #then
 echo "$*"
-echo "$INPUT_report_type"
+echo "$INPUT_package_name"
 chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
 #fi
 #chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
