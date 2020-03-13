10788aab118c28822badca34d57e91ee61cea5e9
ben stear <bjs385@drexel.edu>
2020-03-13 11:36:01 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 set -e
 
+#### this file has abstracted away the type of report and the package for each user. 
+#### I am also going to add the option of specifying which function to use 
+#### whether that be linting, unit testing, build testing or something else.
+#### I think this will be general enough so that we won't have to change this 
+#### file at all on a user to user basis.
+
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
+echo "with PACKAGE_NAME: $INPUT_PACKAGE_NAME
+echo "     Function: linting"
+echo "     Verbosity: $INPUT_VERBOSITY"
 
+# if verbosity!= full_report || score_only, verbosity == default, echo verbosity input incorrect, using default.
 
-
-
-
-if [ "$INPUT_VERBOSITY" == "test" ] 
+if [ "$INPUT_VERBOSITY" == "full_report" ] 
   then
-  #chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
-  echo "you did it"
-  pylint "$INPUT_PACKAGE_NAME"  --exit-zero --reports=y | \
-  awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
+  pylint "$INPUT_PACKAGE_NAME"  --exit-zero --reports=y | \ > pylint-report.txt 
   
-elif [ "$INPUT_VERBOSITY" == "score" ] 
+elif [ "$INPUT_VERBOSITY" == "score_only" ] 
   then
   pylint "$INPUT_PACKAGE_NAME"  --exit-zero --reports=y | \
   awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
-
-
-
-
-
 
 
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
 
-
-#ls -a
-#pylint scedar  --exit-zero --reports=y | \
-#awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
-#ls
 ##chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
