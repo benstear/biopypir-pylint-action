3568246ae730d2fbb47c681d16c7c8319241af9b
ben stear <bjs385@drexel.edu>
2020-03-13 11:13:23 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,29 @@ set -e
 echo "#################################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-if [ "$INPUT_PACKAGE_NAME" == "test" ] 
-then
-chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
-echo "you did it"
-fi
-#chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
+
+
+
+
+
+if [ "$INPUT_VERBOSITY" == "test" ] 
+  then
+  #chmod +x ./docs/biopypir_docs/linting.sh && ./docs/biopypir_docs/linting.sh
+  echo "you did it"
+  pylint "$INPUT_PACKAGE_NAME"  --exit-zero --reports=y | \
+  awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
+  
+elif [ "$INPUT_VERBOSITY" == "score" ] 
+  then
+  pylint "$INPUT_PACKAGE_NAME"  --exit-zero --reports=y | \
+  awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
+
+
+
+
+
+
+
 echo "#################################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
@@ -19,3 +36,4 @@ echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 #pylint scedar  --exit-zero --reports=y | \
 #awk '$0 ~ /Your code/ || $0 ~ /Global/ {print}' | cut -d'/' -f1 | rev | cut -d' ' -f1 | rev > pylint-report.txt 
 #ls
+##chmod +x ./docs/biopypir_docs/test_1.sh && ./docs/biopypir_docs/test_1.sh
